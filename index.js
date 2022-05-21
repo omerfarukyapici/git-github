aa0c2e472f45e325b92eb075285456d5c1d4ca88
omerfarukyapici <omerfarukyapici20@gmail.com>
2022-05-22 02:36:07 +0300

    Added problem 3

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const maxNumber = functionOne(numberArray);
 const maxNumberTwo = functionOne([-10, 0, 1]);
 //console.log(maxNumber, " - ", maxNumberTwo);
 
-// 2 -) Write a function that takes two numbers (a and b) as arguments
-// If a is smaller than b, divide a by b
+// 2 -) Write a function that takes two numbers (a and b) as arguments, 
+// If a is smaller than b, divide a by b 
 // Otherwise, multiply both numbers
 // Return the resulting value
 const functionTwo = (a, b) => {
@@ -25,4 +25,11 @@ const functionTwo = (a, b) => {
     //return a < b  ? a / b : a * b;  <- Another way.
 }
 const ifAB = functionTwo(30, 20);
-console.log(ifAB);
+//console.log(ifAB);
+
+// 3 -) Write a function that takes a value as argument
+// Return the type of the value
+const returnTypeOfArgument = (a) => {
+    return typeof(a);
+}
+console.log(returnTypeOfArgument(1));
